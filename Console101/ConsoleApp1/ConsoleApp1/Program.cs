ad6a567ccea1b18ba700eaf01fb12bd5e478bbf1
james-hala <halajameshala@gmail.com>
2025-04-27 03:33:40 +0800

    vs2022practice

---
+++ Console101/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,195 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        #region 原版
+        //// 原版
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello, World!");
+        //}
+        #endregion
+        #region 範例1
+        //// 範例1
+        //// 摘要:
+        //// 1. 程式以區塊加註解文字可增加可讀性, 註解文字也可以在程式碼後面添加.
+        ///     註解文字可以 // 開始, 也可以 /* 註解文字 */ 方式撰寫.
+        ///     註解文字可用上方 Text Editor 工具列 快速開啟或關閉. 
+        //// 2. 以 VisualStudio IDE、檔案總管、Console 分別執行測試, 比較不同執行環境的差異結果.
+        //// 3. 測試錯誤輸入的狀況: 例如出生年輸入字串.
+        ////    不管是程式或是作業系統, 應能成功執行完 Input-Process-Output 3個區塊. 
+        ////    就算是無法執行到結束, 也不可以當機或中斷跳出.  
+        //static void Main(string[] args)
+        //{
+        //    // Input
+        //    Console.Write("名稱:");
+        //    string? sName = Console.ReadLine();
+
+        //    Console.Write("出生年:");
+        //    string? sYear = Console.ReadLine();
+        //    if (string.IsNullOrEmpty(sYear))
+        //        return;
+
+        //    // Process
+        //    int iAge = DateTime.Now.Year - int.Parse(sYear);
+
+
+        //    // Output
+        //    string sOutput = $"Hello, {sName} 你今年 {iAge} 歲";
+        //    Console.WriteLine(sOutput);
+
+        //    // UI: User Interface handlling.
+        //    // 讓使用者看到上方的結果, 確認後再結束.
+        //    Console.WriteLine(); // User friendly: 空一行比較易讀美觀.
+        //    Console.WriteLine("按 Enter鍵結束");
+        //    Console.ReadLine();
+        //}
+        #endregion
+
+        #region 範例2
+        /*
+        範例2: 解決錯誤輸入的狀況: 例如出生年輸入字串.
+        摘要:
+        1. 註解文字也可以頭尾方式撰寫, 不用每一列加 // 註解.
+        2. 迴圈介紹.
+        3. Try-Catch 錯誤處理專門解決(無法預期的錯誤).
+           無法預期的錯誤通常是指會導致(程式或作業系統)無法執行到結束.
+         */
+        static void Main(string[] args)
+        {
+            // Input
+            Console.Write("名稱:");
+            string? sName = Console.ReadLine();
+
+            // Process
+            int _Year = 0;
+            int iThisYear = DateTime.Now.Year;
+            while (true) // 迴圈開始
+            {
+                Console.Write("出生年:");
+                string? sYear = Console.ReadLine();
+                if (string.IsNullOrEmpty(sYear))
+                    return;
+
+                // Try-Catch 錯誤處理專門解決(無法預期的錯誤).
+                try
+                {
+                    _Year = int.Parse(sYear);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"請輸入數字, 因為發生錯誤={ex.Message}.");
+                    Console.WriteLine(); // User friendly: 空一行比較易讀美觀.
+                    continue; // 下一個指令從迴圈開始執行.
+                }
+
+                // (可以預期的錯誤)是開發人員應處理的範圍.
+                if (_Year < 1900 || _Year > iThisYear)
+                {
+                    Console.WriteLine($"請輸入正確的出生年, 1900~{iThisYear}.");
+                    continue; // 下一個指令從迴圈開始執行.
+                }
+
+                // 這裡的 iYear 是正確的, 可以用來計算年齡.
+                break; // 終止迴圈. 執行迴圈後的下一個指令
+            }
+            int iAge = DateTime.Now.Year - _Year;
+
+
+            // Output
+            string sOutput = $"Hello, {sName} 你今年 {iAge} 歲";
+            Console.WriteLine(sOutput);
+
+            // UI: User Interface handlling.
+            // 讓使用者看到上方的結果, 確認後再結束.
+            Console.WriteLine(); // User friendly: 空一行比較易讀美觀.
+            Console.WriteLine("按 Enter鍵結束");
+            Console.ReadLine();
+        }
+        #endregion
+
+        #region 練習1
+        //// 練習1: 將 Input 改為以參數字串陣列 string[] args 輸入.
+        ////摘要:
+        //// 參數字串陣列 string[] args, 若在 IDE 環境中執行時, 必須在 (Project).Properties.Debug.General.Launch Profiles.Command line Arguments 欄位中輸入. 例如:  林襄 2001
+        //static void Main(string[] args)
+        //{
+        //    // Input
+        //    string? sProgramName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name; // 取得程式名稱
+        //    string sHelp1 = $"請輸入名稱和出生年, 並且以空白間隔. 例如: {sProgramName} '林襄 2001'."; // 共用的資源應盡量整合.
+        //    if (args.Length < 2)
+        //    {
+        //        Console.WriteLine(sHelp1);
+        //        return;
+        //    }
+        //    string sName = args[0];
+        //    string sYear = args[1];
+
+        //    // Process
+        //    if (int.TryParse(sYear, out int iYear) == false)
+        //    {
+        //        Console.WriteLine(sHelp1);
+        //        return;
+        //    }
+        //    int iThisYear = DateTime.Now.Year;
+        //    if (iYear < 1900 || iYear > iThisYear)
+        //    {
+        //        Console.WriteLine($"請輸入正確的出生年, 1900~{iThisYear}.");
+        //        return;
+        //    }
+        //    int iAge = DateTime.Now.Year - iYear;
+
+        //    // Output
+        //    string sOutput = $"Hello, {sName} 你今年 {iAge} 歲";
+        //    Console.WriteLine(sOutput);
+        //}
+        #endregion
+
+        #region 練習2
+        //// 練習2: 將 Input 改為以Environment.CommandLine 輸入.
+        ////摘要:
+        //// Environment.CommandLine 是唯讀的屬性, 無法輸入, 只能在(非 IDE 執行環境)中輸入.
+        //static void Main()
+        //{
+        //    try
+        //    {
+        //        // Input
+        //        string? sProgramName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name; // 取得程式名稱
+        //        string sHelp1 = $"請輸入名稱和出生年, 並且以空白間隔. 例如: {sProgramName} '林襄 2001'."; // 共用的資源應盡量整合.
+        //        if (Environment.CommandLine.Length < 1)
+        //        {
+        //            Console.WriteLine(sHelp1);
+        //            return;
+        //        }
+        //        string[] args = Environment.CommandLine.Split(' ');
+        //        string sName = args[0];
+        //        string sYear = args[1];
+
+        //        // Process
+        //        if (int.TryParse(sYear, out int iYear) == false)
+        //        {
+        //            Console.WriteLine(sHelp1);
+        //            return;
+        //        }
+        //        int iThisYear = DateTime.Now.Year;
+        //        if (iYear < 1900 || iYear > iThisYear)
+        //        {
+        //            Console.WriteLine($"請輸入正確的出生年, 1900~{iThisYear}.");
+        //            return;
+        //        }
+        //        int iAge = DateTime.Now.Year - iYear;
+
+        //        // Output
+        //        string sOutput = $"Hello, {sName} 你今年 {iAge} 歲";
+        //        Console.WriteLine(sOutput);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // 已經交付給使用者使用的程式, 不能當機或中斷跳出.
+        //        Console.WriteLine($"發生無法預期的錯誤={ex.Message}.");
+        //        return;
+        //    }
+        //}
+        #endregion
+    }
+}
---
+++ Lab2-WinForm/WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,469 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            TxtName = new TextBox();
+            TxtPrice = new TextBox();
+            TxtQty = new TextBox();
+            TxtAmount = new TextBox();
+            LblTotalQty = new Label();
+            LblTotalAmt = new Label();
+            LblAvg = new Label();
+            label13 = new Label();
+            LblNo = new Label();
+            txtSKU = new TextBox();
+            LblSkuNo = new Label();
+            listBox1 = new ListBox();
+            listView1 = new ListView();
+            BtnAdd = new Button();
+            BtnDelete = new Button();
+            BtnUpdate = new Button();
+            BtnClear = new Button();
+            BtnBuildTestData = new Button();
+            label9 = new Label();
+            LblLengthOfName = new Label();
+            LblLengthOfBytes = new Label();
+            label11 = new Label();
+            ChkProductionEnv = new CheckBox();
+            groupBoxTest = new GroupBox();
+            LblMsg = new Label();
+            LblCount = new Label();
+            label14 = new Label();
+            groupBox1 = new GroupBox();
+            groupBoxTest.SuspendLayout();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(32, 82);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 19);
+            label1.TabIndex = 0;
+            label1.Text = "名稱";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(32, 115);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 19);
+            label2.TabIndex = 1;
+            label2.Text = "單價";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(32, 148);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 19);
+            label3.TabIndex = 2;
+            label3.Text = "數量";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(32, 181);
+            label4.Name = "label4";
+            label4.Size = new Size(39, 19);
+            label4.TabIndex = 3;
+            label4.Text = "金額";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(9, 34);
+            label6.Name = "label6";
+            label6.Size = new Size(39, 19);
+            label6.TabIndex = 5;
+            label6.Text = "數量";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(9, 67);
+            label7.Name = "label7";
+            label7.Size = new Size(39, 19);
+            label7.TabIndex = 6;
+            label7.Text = "金額";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(9, 100);
+            label8.Name = "label8";
+            label8.Size = new Size(39, 19);
+            label8.TabIndex = 7;
+            label8.Text = "平均";
+            // 
+            // TxtName
+            // 
+            TxtName.Location = new Point(76, 77);
+            TxtName.Name = "TxtName";
+            TxtName.Size = new Size(120, 27);
+            TxtName.TabIndex = 8;
+            // 
+            // TxtPrice
+            // 
+            TxtPrice.Location = new Point(76, 110);
+            TxtPrice.Name = "TxtPrice";
+            TxtPrice.Size = new Size(120, 27);
+            TxtPrice.TabIndex = 9;
+            // 
+            // TxtQty
+            // 
+            TxtQty.Location = new Point(76, 143);
+            TxtQty.Name = "TxtQty";
+            TxtQty.Size = new Size(120, 27);
+            TxtQty.TabIndex = 10;
+            // 
+            // TxtAmount
+            // 
+            TxtAmount.Location = new Point(76, 176);
+            TxtAmount.Name = "TxtAmount";
+            TxtAmount.Size = new Size(120, 27);
+            TxtAmount.TabIndex = 11;
+            // 
+            // LblTotalQty
+            // 
+            LblTotalQty.AutoSize = true;
+            LblTotalQty.Location = new Point(53, 32);
+            LblTotalQty.Name = "LblTotalQty";
+            LblTotalQty.Size = new Size(60, 19);
+            LblTotalQty.TabIndex = 13;
+            LblTotalQty.Text = "label10";
+            // 
+            // LblTotalAmt
+            // 
+            LblTotalAmt.AutoSize = true;
+            LblTotalAmt.Location = new Point(53, 65);
+            LblTotalAmt.Name = "LblTotalAmt";
+            LblTotalAmt.Size = new Size(60, 19);
+            LblTotalAmt.TabIndex = 14;
+            LblTotalAmt.Text = "label11";
+            // 
+            // LblAvg
+            // 
+            LblAvg.AutoSize = true;
+            LblAvg.Location = new Point(53, 98);
+            LblAvg.Name = "LblAvg";
+            LblAvg.Size = new Size(60, 19);
+            LblAvg.TabIndex = 15;
+            LblAvg.Text = "label12";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(32, 19);
+            label13.Name = "label13";
+            label13.Size = new Size(39, 19);
+            label13.TabIndex = 16;
+            label13.Text = "項目";
+            // 
+            // LblNo
+            // 
+            LblNo.AutoSize = true;
+            LblNo.Location = new Point(77, 19);
+            LblNo.Name = "LblNo";
+            LblNo.Size = new Size(60, 19);
+            LblNo.TabIndex = 17;
+            LblNo.Text = "label14";
+            // 
+            // txtSKU
+            // 
+            txtSKU.Location = new Point(76, 44);
+            txtSKU.Name = "txtSKU";
+            txtSKU.Size = new Size(120, 27);
+            txtSKU.TabIndex = 19;
+            // 
+            // LblSkuNo
+            // 
+            LblSkuNo.AutoSize = true;
+            LblSkuNo.Location = new Point(32, 49);
+            LblSkuNo.Name = "LblSkuNo";
+            LblSkuNo.Size = new Size(38, 19);
+            LblSkuNo.TabIndex = 18;
+            LblSkuNo.Text = "SKU";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 19;
+            listBox1.Location = new Point(202, 44);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(538, 156);
+            listBox1.TabIndex = 20;
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(202, 218);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(538, 159);
+            listView1.TabIndex = 21;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // BtnAdd
+            // 
+            BtnAdd.Location = new Point(2, 428);
+            BtnAdd.Name = "BtnAdd";
+            BtnAdd.Size = new Size(94, 29);
+            BtnAdd.TabIndex = 22;
+            BtnAdd.Text = "新增";
+            BtnAdd.UseVisualStyleBackColor = true;
+            BtnAdd.Click += BtnAdd_Click;
+            // 
+            // BtnDelete
+            // 
+            BtnDelete.Location = new Point(102, 429);
+            BtnDelete.Name = "BtnDelete";
+            BtnDelete.Size = new Size(94, 29);
+            BtnDelete.TabIndex = 23;
+            BtnDelete.Text = "刪除";
+            BtnDelete.UseVisualStyleBackColor = true;
+            BtnDelete.Click += BtnDelete_Click;
+            // 
+            // BtnUpdate
+            // 
+            BtnUpdate.Location = new Point(202, 429);
+            BtnUpdate.Name = "BtnUpdate";
+            BtnUpdate.Size = new Size(94, 29);
+            BtnUpdate.TabIndex = 24;
+            BtnUpdate.Text = "修改";
+            BtnUpdate.UseVisualStyleBackColor = true;
+            BtnUpdate.Click += BtnUpdate_Click;
+            // 
+            // BtnClear
+            // 
+            BtnClear.Location = new Point(302, 429);
+            BtnClear.Name = "BtnClear";
+            BtnClear.Size = new Size(94, 29);
+            BtnClear.TabIndex = 25;
+            BtnClear.Text = "清除";
+            BtnClear.UseVisualStyleBackColor = true;
+            // 
+            // BtnBuildTestData
+            // 
+            BtnBuildTestData.Location = new Point(14, 95);
+            BtnBuildTestData.Name = "BtnBuildTestData";
+            BtnBuildTestData.Size = new Size(94, 29);
+            BtnBuildTestData.TabIndex = 26;
+            BtnBuildTestData.Text = "測試資料";
+            BtnBuildTestData.UseVisualStyleBackColor = true;
+            BtnBuildTestData.Click += BtnBuildTestData_Click;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(7, 33);
+            label9.Name = "label9";
+            label9.Size = new Size(84, 19);
+            label9.TabIndex = 27;
+            label9.Text = "名稱字元數";
+            // 
+            // LblLengthOfName
+            // 
+            LblLengthOfName.AutoSize = true;
+            LblLengthOfName.Location = new Point(104, 32);
+            LblLengthOfName.Name = "LblLengthOfName";
+            LblLengthOfName.Size = new Size(60, 19);
+            LblLengthOfName.TabIndex = 28;
+            LblLengthOfName.Text = "label12";
+            // 
+            // LblLengthOfBytes
+            // 
+            LblLengthOfBytes.AutoSize = true;
+            LblLengthOfBytes.Location = new Point(104, 61);
+            LblLengthOfBytes.Name = "LblLengthOfBytes";
+            LblLengthOfBytes.Size = new Size(60, 19);
+            LblLengthOfBytes.TabIndex = 30;
+            LblLengthOfBytes.Text = "label12";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(5, 61);
+            label11.Name = "label11";
+            label11.Size = new Size(91, 19);
+            label11.TabIndex = 29;
+            label11.Text = "名稱bytes數";
+            // 
+            // ChkProductionEnv
+            // 
+            ChkProductionEnv.AutoSize = true;
+            ChkProductionEnv.Location = new Point(749, 49);
+            ChkProductionEnv.Name = "ChkProductionEnv";
+            ChkProductionEnv.Size = new Size(91, 23);
+            ChkProductionEnv.TabIndex = 31;
+            ChkProductionEnv.Text = "正式環境";
+            ChkProductionEnv.UseVisualStyleBackColor = true;
+            ChkProductionEnv.CheckedChanged += ChkProductionEnv_CheckedChanged;
+            // 
+            // groupBoxTest
+            // 
+            groupBoxTest.Controls.Add(LblLengthOfName);
+            groupBoxTest.Controls.Add(BtnBuildTestData);
+            groupBoxTest.Controls.Add(LblLengthOfBytes);
+            groupBoxTest.Controls.Add(label9);
+            groupBoxTest.Controls.Add(label11);
+            groupBoxTest.Location = new Point(749, 80);
+            groupBoxTest.Name = "groupBoxTest";
+            groupBoxTest.Size = new Size(217, 140);
+            groupBoxTest.TabIndex = 32;
+            groupBoxTest.TabStop = false;
+            groupBoxTest.Text = "測試環境需要的";
+            // 
+            // LblMsg
+            // 
+            LblMsg.AutoSize = true;
+            LblMsg.Location = new Point(12, 406);
+            LblMsg.Name = "LblMsg";
+            LblMsg.Size = new Size(69, 19);
+            LblMsg.TabIndex = 33;
+            LblMsg.Text = "訊息結果";
+            // 
+            // LblCount
+            // 
+            LblCount.AutoSize = true;
+            LblCount.Location = new Point(248, 380);
+            LblCount.Name = "LblCount";
+            LblCount.Size = new Size(60, 19);
+            LblCount.TabIndex = 35;
+            LblCount.Text = "label12";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(204, 382);
+            label14.Name = "label14";
+            label14.Size = new Size(39, 19);
+            label14.TabIndex = 34;
+            label14.Text = "筆數";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(LblTotalQty);
+            groupBox1.Controls.Add(LblTotalAmt);
+            groupBox1.Controls.Add(LblAvg);
+            groupBox1.Location = new Point(12, 252);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(184, 125);
+            groupBox1.TabIndex = 36;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "合計";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(984, 470);
+            Controls.Add(groupBox1);
+            Controls.Add(LblCount);
+            Controls.Add(label14);
+            Controls.Add(LblMsg);
+            Controls.Add(groupBoxTest);
+            Controls.Add(ChkProductionEnv);
+            Controls.Add(BtnClear);
+            Controls.Add(BtnUpdate);
+            Controls.Add(BtnDelete);
+            Controls.Add(BtnAdd);
+            Controls.Add(listView1);
+            Controls.Add(listBox1);
+            Controls.Add(txtSKU);
+            Controls.Add(LblSkuNo);
+            Controls.Add(LblNo);
+            Controls.Add(label13);
+            Controls.Add(TxtAmount);
+            Controls.Add(TxtQty);
+            Controls.Add(TxtPrice);
+            Controls.Add(TxtName);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            groupBoxTest.ResumeLayout(false);
+            groupBoxTest.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private TextBox TxtName;
+        private TextBox TxtPrice;
+        private TextBox TxtQty;
+        private TextBox TxtAmount;
+        private Label LblTotalQty;
+        private Label LblTotalAmt;
+        private Label LblAvg;
+        private Label label13;
+        private Label LblNo;
+        private TextBox txtSKU;
+        private Label LblSkuNo;
+        private ListBox listBox1;
+        private ListView listView1;
+        private Button BtnAdd;
+        private Button BtnDelete;
+        private Button BtnUpdate;
+        private Button BtnClear;
+        private Button BtnBuildTestData;
+        private Label label9;
+        private Label LblLengthOfName;
+        private Label LblLengthOfBytes;
+        private Label label11;
+        private CheckBox ChkProductionEnv;
+        private GroupBox groupBoxTest;
+        private Label LblMsg;
+        private Label LblCount;
+        private Label label14;
+        private GroupBox groupBox1;
+    }
+}
